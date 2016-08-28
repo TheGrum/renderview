fb97abe9819b52178dc536729146d20b67e97597
Howard C. Shaw III <howardcshaw@gmail.com>
2016-08-28 15:49:45 -0500

    Tweaking build tags.

--- driver/driver_gtk2.go
+++ driver/driver_gtk2.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT-license
 // as defined in the LICENSE file.
 
-// +build !gotk3,!android,!shiny
+// +build !nogtk2,!gotk3 !nogtk2,!android !nogtk2,!shiny
 
 package driver
 
--- driver/gtk2/view.go
+++ driver/gtk2/view.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT-license
 // as defined in the LICENSE file.
 
-// +build !gotk3,!shiny
+// +build !gotk3,!nogtk2 !shiny,!nogtk2
 
 package gtk2
 
