097a3a97eba60293c2c43202f43e8b3a34d684ab
Howard C. Shaw III <howardcshaw@gmail.com>
2020-02-19 19:56:01 -0600

    Corrected copyright dates on new files.

--- driver/driver_gio.go
+++ driver/driver_gio.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Howard C. Shaw III. All rights reserved.
+// Copyright 2020 Howard C. Shaw III. All rights reserved.
 // Use of this source code is governed by the MIT-license
 // as defined in the LICENSE file.
 
